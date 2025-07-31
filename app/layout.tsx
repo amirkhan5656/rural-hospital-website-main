f1bcdabd432e4916c96c182c30c64e4814c60429
amirkhan5656 <amirqureshi590590@gmail.com>
2025-07-31 13:46:35 +0500

    second commit, correct the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,31 @@
-<<<<<<< HEAD
-import type { Metadata } from 'next'
-=======
-import type { Metadata } from '@/rural-hospital-website/node_modules/next'
->>>>>>> f0fd36a (Update the page.tsx)
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "v0 App",
+  description: "Created with v0",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
         <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
+          html {
+            font-family: ${GeistSans.style.fontFamily};
+            --font-sans: ${GeistSans.variable};
+            --font-mono: ${GeistMono.variable};
+          }
         `}</style>
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
